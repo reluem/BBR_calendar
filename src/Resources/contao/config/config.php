736ba1d5e76e8b45b7e9b7112afc51063fae7eff
reluem <lucas.rech@me.com>
2020-03-29 12:05:17 +0200

    update requirements and class loading

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,9 +1,10 @@
 <?php
     
     
-    
     $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['events_subscriptions']['events_subscriptions_subscribe']['attachment_tokens'] = ['ics_file'];
     
-    \Codefog\EventsSubscriptions\Services::getSubscriptionFactory()->add(
+    use Codefog\EventsSubscriptions\Services;
+    
+    Services::getSubscriptionFactory()->add(
         'guest', \Reluem\BBRCalendarBundle\guestSubscriptionExt::class
     );
